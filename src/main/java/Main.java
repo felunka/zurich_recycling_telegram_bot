618675fa5d2bf5b76d36d11fb2b061014311dd64
felunka <felixvontiedemann@gmail.com>
2020-01-12 14:39:29 +0100

    Change bot to ask for command parameter and add more commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,14 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
-import com.sun.tools.doclint.Messages;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -18,8 +18,10 @@ public class Main {
     static TelegramBot bot;
 
     enum RecyclingGroup {
+        WASTE,
         PAPER,
         CARDBOARD,
+        CARGOTRAM,
         ORGANIC,
         TEXTILE
     }
@@ -31,27 +33,64 @@ public class Main {
         // Create your bot passing the token received from @BotFather
         bot = new TelegramBot("812487487:AAH6aULaTpT8wJeHWJ6vdI-HlUnvyttsBxE");
 
-        Pattern commandPattern = Pattern.compile("/(\\w+)\\s(\\w+)");
+        HashMap<Long, String> session = new HashMap<Long, String>();
+
+        Pattern commandPattern = Pattern.compile("/(\\w+)");
 
         // Register for updates
         bot.setUpdatesListener(updates -> {
             for(Update update : updates) {
+                if(update.message() == null) {
+                    continue;
+                }
+
                 System.out.println(update.message().text());
 
                 long chatId = update.message().chat().id();
+
+                if(update.message().text() == null) {
+                    bot.execute(new SendMessage(chatId, "Sorry, I can only handle text... :("));
+                    continue;
+                }
 
                 Matcher matcher = commandPattern.matcher(update.message().text());
                 if(matcher.matches()) {
                     switch (matcher.group(1)) {
                         case "sub":
-                            SendResponse response = bot.execute(new SendMessage(chatId, subscribe(chatId, matcher.group(2))));
+                            session.put(chatId, matcher.group(1));
+                            bot.execute(new SendMessage(chatId, "Tell me, what you want to subscribe to."));
+                            break;
+                        case "unsub":
+                            session.put(chatId, matcher.group(1));
+                            bot.execute(new SendMessage(chatId, "What group do you want to unsubscribe?"));
+                            break;
+                        case "mysubs":
+                            bot.execute(new SendMessage(chatId, showSubs(chatId)));
                             break;
                         case "location":
-                            bot.execute(new SendMessage(chatId, location(chatId, matcher.group(2))));
+                            session.put(chatId, matcher.group(1));
+                            bot.execute(new SendMessage(chatId, "Tell me you zip code."));
+                            break;
+                        case "list":
+                            String list = Arrays.stream(RecyclingGroup.values()).map(t -> t.name().toLowerCase()).collect(Collectors.joining(", "));
+                            bot.execute(new SendMessage(chatId, String.format("You can subscribe to: %s.", list)));
                             break;
                         case "debugCheck":
                             sendNotifications();
                             break;
+                    }
+                } else {
+                    if(session.containsKey(chatId)) {
+                        switch (session.get(chatId)) {
+                            case "sub":
+                                bot.execute(new SendMessage(chatId, subscribe(chatId, update.message().text())));
+                                break;
+                            case "unsub":
+                                bot.execute(new SendMessage(chatId, unsubscribe(chatId, update.message().text())));
+                                break;
+                            case "location":
+                                bot.execute(new SendMessage(chatId, location(chatId, update.message().text())));
+                        }
                     }
                 }
             }
@@ -60,6 +99,26 @@ public class Main {
         });
 
         new Scheduler();
+    }
+
+    private static String showSubs(long chatId) {
+        StringBuilder sb = new StringBuilder();
+
+        ResultSet subs = database.queryWithResult(String.format("SELECT subscriptions.groupName FROM subscriptions WHERE chatID LIKE '%d'", chatId));
+        try {
+            while(subs.next())
+            {
+                RecyclingGroup groupEnum = RecyclingGroup.valueOf(subs.getString("groupName"));
+
+                if(sb.length() != 0)
+                    sb.append(", ");
+
+                sb.append(groupEnum.name().toLowerCase());
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return String.format("You are subscribed to: %s", sb.toString());
     }
 
     private static String location(long chatID, String zip) {
@@ -87,6 +146,21 @@ public class Main {
         }
     }
 
+    private static String unsubscribe(long chatID, String group) {
+        try {
+            RecyclingGroup.valueOf(group.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return "Invalid recycling group chosen!";
+        }
+
+        boolean result = database.query(String.format("DELETE FROM subscriptions WHERE groupName LIKE '%s' AND chatID LIKE '%d'", group, chatID));
+        if(result) {
+            return String.format("You are now on your own, so don't blame me if you forget to bring out %s...", group);
+        } else {
+            return "Fail!";
+        }
+    }
+
     public static void sendNotifications() {
         HashMap<String, Boolean> cache = new HashMap<>();
         ErzApi api = new ErzApi();
@@ -105,6 +179,7 @@ public class Main {
                     notify = cache.get(groupEnum.name() + zip);
                 } else {
                     notify = api.checkNotify(groupEnum, zip);
+                    cache.put(groupEnum.name() + zip, notify);
                 }
 
                 System.out.println(String.format("%d will get notification? %b", subs.getLong("chatID"), notify));
@@ -116,8 +191,5 @@ public class Main {
         } catch(SQLException e) {
             e.printStackTrace();
         }
-
     }
-
-
 }
